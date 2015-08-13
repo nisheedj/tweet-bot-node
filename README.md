41c53da294c826f904386b708d85e7e9225117b2
nisheedj <nisheedj@thoughtworks.com>
2015-08-13 17:08:01 +0530

    Updated readme and moved config out of the app.js file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tweet-bot-node
 Twitter Bot using node js , RPi , Arduino, Jonny Five
+
+## Config file `twitter-config.json`
+We need to create a `twitter-config.json` file which contains the following json object.
+
+```javascript
+{
+  "consumer_key": "<consumer_key>",
+  "consumer_secret": "<consumer_secret>",
+  "access_token_key": "<access_token_key>",
+  "access_token_secret": "<access_token_secret>"
+}
+```
+
+__NOTE__ Failure to create the `.json` file will cause an error while running the code.
--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var twitter = require('twitter');
+var twitterConfig = require('./twitter-config.json');
 
-var client = new twitter({
-  consumer_key: 'mBGCD0AjU04765KnDNXYfL7Rd',
-  consumer_secret: '87YsapwGWtg6Zmj4r9A8PLzEalqHRnl0FkfV8YOWhaAJZgE347',
-  access_token_key: '314971212-OU4wzR5DbUNLxoYtIuOCmeBvjMuC2ADlsZT5q0xP',
-  access_token_secret: 'chDRCK4tq47QyPP2LEsPjaR9mOttbHKO0ZEUc3z9xOBmJ'
-});
 
-client.stream('statuses/filter', {track: '@nishDroid'}, function(stream) {
+var client = new twitter(twitterConfig);
+
+client.stream('statuses/filter', {track: '@awaydaybot'}, function(stream) {
   
   stream.on('data', function(tweet) {
     console.log(tweet.text);
